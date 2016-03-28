e5046132716f055488f22735a4cdd2e01de5978b
altostratous <altostratous@gmail.com>
2016-03-28 19:45:40 +0430

    Done some permission games.

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import *
 from .models import *
 
 # Register your models here.
@@ -7,3 +8,4 @@ admin.site.register(Workflow)
 admin.site.register(Step)
 admin.site.register(Order)
 admin.site.register(Profile)
+admin.site.register(Permission)
--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 # Create your views here.
 
 from django.http import HttpResponse
 
 
+# @permission_required('add_user')
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
