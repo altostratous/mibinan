281dde0b83688e33dff192610be8d8bbef95062c
altostratous <altostratous@gmail.com>
2016-03-28 09:10:17 +0430

    Added order model and added some initial values to database
    Registered all models in the django admin

--- main/models.py
+++ main/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class Service(models.Model):
     title = models.CharField(max_length=256)
     description = models.TextField()
-    parent_service = models.ForeignKey('self')
+    parent_service = models.ForeignKey('self', blank=True, null=True, default=0)
     cost = models.IntegerField()
     workflow = models.ForeignKey('Workflow')
     step_order = models.IntegerField()
@@ -15,8 +16,17 @@ class Service(models.Model):
 class Workflow(models.Model):
     title = models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.title
+
 
 class Step(models.Model):
     title = models.CharField(max_length=256)
     workflow = models.ForeignKey('Workflow')
     order = models.IntegerField()
+
+
+class Order(models.Model):
+    user = models.ForeignKey('auth.User')
+    created = models.DateTimeField(auto_now_add=True)
+    service_set = models.ManyToManyField("Service")
