2868a053b50ae2ea4c9bd2e619cc1cda1b1270f7
altostratous <altostratous@gmail.com>
2016-03-29 18:24:21 +0430

    Added a simple form for getting familiar with django forms.

--- main/views.py
+++ main/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import permission_required
-# Create your views here.
-
+from .forms import *
 from django.http import HttpResponse
+
+# from django.contrib.auth.decorators import permission_required
+# Create your views here.
 
 
 # @permission_required('add_user')
 def index(request):
-    return render(request, 'main/simpleform.html', {'title_for_layout': 'hello',
-                                              'nav_bar': {'اول': 'hello', 'دوم': 'hello'}})
+    if request.method == 'POST':
+        form = ServiceForm(request.POST)
+        return HttpResponse(form)
+    else:
+        form = ServiceForm()
+        return render(request, 'main/simpleform.html', {'title_for_layout': 'hello',
+                                                        'form': form,
+                                                        'nav_bar': {'اول': 'hello', 'دوم': 'hello'}})
