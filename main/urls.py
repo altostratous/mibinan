689f8608bdc8cf9c7c28f864007a7a0d6e1e540d
altostratous <altostratous@gmail.com>
2016-04-01 00:40:35 +0430

    Added orders index action.

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,6 @@ from .controllers import orders
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^orders/add*$', orders.orders_add, name='orders_add'),
+    url(r'^orders/add$', orders.orders_add, name='orders_add'),
     url(r'^orders$', orders.index, name='orders'),
 ]
